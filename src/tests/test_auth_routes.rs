f9506d1ed1b0fe14448653955de48a24883a5789
Edimar Freitas de Sá <edimar.sa@efscode.com.br>
2024-03-26 18:01:38 -0300

    Criados testes para autenticação falhando e autenticação por JWT

--- src/tests/test_auth_routes.rs
+++ src/tests/test_auth_routes.rs
@@ -1,27 +1,55 @@
-use std::sync::Mutex;
+use std::sync::{Mutex};
+use axum_test::http::{header, StatusCode};
 use axum_test::TestServer;
-use serde_json::json;
+use serde_json::{json, Value};
 use crate::auth::jwt_auth::Token;
 
 use crate::auth::urls::create_routes;
 
-static JWT_INFO: Mutex<Option<String>> = Mutex::new(None);
+static JWT_INFO: Mutex<Option<Token>> = Mutex::new(None);
 
+static URL:&str = "/login";
+
+fn setup_test_server() -> TestServer {
+    TestServer::new(create_routes()).unwrap()
+}
 
 #[tokio::test]
 async fn test_authentication_success() {
-
-    let url = "/login";
     let auth = json!({
         "email": "example@example.com",
         "password": "P@ssword123",
     });
-
-    let server = TestServer::new(create_routes()).unwrap();
-    let response = server.post(url).json(&auth).await;
-
+    let response = setup_test_server().post(URL).json(&auth).await;
 
     assert_eq!(response.status_code(), 200);
 
-    *JWT_INFO.lock().unwrap() = Some(response.json::<Token>().token);
+    *JWT_INFO.lock().unwrap() = Some(response.json::<Token>());
+}
+
+#[tokio::test]
+async fn test_authentication_failure() {
+    let auth = json!({
+        "email": "example@example.com",
+        "password": "password",
+    });
+    let response = setup_test_server().post(URL).json(&auth).await;
+
+    assert_eq!(response.status_code(), 400);
+}
+
+#[tokio::test]
+async fn test_authentication_with_jwt() {
+    let token = JWT_INFO.lock().unwrap().clone().unwrap();
+
+    let json_ = json!({"id": token.id});
+
+    let url = String::from_iter([URL, "/jwt"]);
+
+    let response = setup_test_server().post(&url)
+        .add_header(header::AUTHORIZATION, format!("Bearer {}", token.token).parse().unwrap())
+        .json(&json_)
+        .await;
+
+    response.assert_status(StatusCode::ACCEPTED)
 }

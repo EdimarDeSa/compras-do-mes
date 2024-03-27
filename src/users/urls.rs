9c1cc2bed0e5b12bc9ea6c7653da32cfc52b1484
Edimar Freitas de Sá <edimar.sa@efscode.com.br>
2024-03-27 10:13:02 -0300

    Criado endpoint update para usuários.

--- src/users/urls.rs
+++ src/users/urls.rs
@@ -1,7 +1,7 @@
 use axum::{
     extract::Path,
     http::StatusCode,
-    routing::{delete, get, post},
+    routing::{delete, get, post, put},
     Json, Router,
 };
 use serde::Serialize;
@@ -12,7 +12,9 @@ use crate::users::{
     read_user,
     {create_user, create_user::CreationError, user_models::NewUser},
     {delete_user, delete_user::DeletionError},
+    {update_user, update_user::UpdateError}
 };
+use crate::users::user_models::AlterUser;
 
 #[derive(Debug, Serialize)]
 struct ErrorResponse {
@@ -111,10 +113,68 @@ async fn get_user_with_id(id: Path<Uuid>) -> (StatusCode, Json<Value>) {
     }
 }
 
+async fn update(alter_user: Json<AlterUser>) -> (StatusCode, Json<Value>) {
+    match update_user::update(&alter_user) {
+        Ok(u) => (StatusCode::OK, Json(json!(u))),
+        Err(err) => match err {
+            UpdateError::TransactionError(e) => {
+                let msg = ErrorResponse {
+                    error: "TransactionError".to_string(),
+                    msg: e
+                };
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(json!(msg)))
+            },
+            UpdateError::DateFormatError(e) => {
+                let msg = ErrorResponse {
+                    error: "DateFormatError".to_string(),
+                    msg: e
+                };
+                (StatusCode::BAD_REQUEST, Json(json!(msg)))
+            },
+            UpdateError::InvalidPassword(e) => {
+                let msg = ErrorResponse {
+                    error: "InvalidPassword".to_string(),
+                    msg: e
+                };
+                (StatusCode::BAD_REQUEST, Json(json!(msg)))
+            },
+            UpdateError::InvalidEmail(e) => {
+                let msg = ErrorResponse {
+                    error: "InvalidEmail".to_string(),
+                    msg: e
+                };
+                (StatusCode::BAD_REQUEST, Json(json!(msg)))
+            },
+            UpdateError::InvalidFieldError(e) => {
+                let msg = ErrorResponse {
+                    error: "InvalidFieldError".to_string(),
+                    msg: e
+                };
+                (StatusCode::BAD_REQUEST, Json(json!(msg)))
+            },
+            UpdateError::EncryptionError(e) => {
+                let msg = ErrorResponse {
+                    error: "EncryptionError".to_string(),
+                    msg: e
+                };
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(json!(msg)))
+            },
+            UpdateError::UserNotFound => {
+                let msg = ErrorResponse {
+                    error: "UserNotFound".to_string(),
+                    msg: "User not found".to_string()
+                };
+                (StatusCode::NOT_FOUND, Json(json!(msg)))
+            },
+        }
+    }
+}
+
 pub fn create_routes() -> Router {
     Router::new()
         .route("/create", post(create))
         .route("/delete/:user_id", delete(remove))
         .route("/search/email/:email", get(get_user_with_email))
         .route("/search/id/:id", get(get_user_with_id))
+        .route("/update", put(update))
 }

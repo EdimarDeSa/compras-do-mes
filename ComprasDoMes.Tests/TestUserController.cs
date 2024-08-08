4362d24a000a8e01c664b96fa778040ea7c0dc8e
Edimar Sá <edimar.sa@efscode.com.br>
2024-08-07 23:17:56 -0300

    Updated tests for new structure of controller and internacionalization

--- ComprasDoMes.Tests/TestUserController.cs
+++ ComprasDoMes.Tests/TestUserController.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Moq;
 using ComprasDoMes.Controllers;
 using ComprasDoMes.Models;
 using ComprasDoMes.Models.UserModel;
+using ComprasDoMes.Models.Internacionalizations;
 
 public class UserControllerTests
 {
     private readonly ComprasDoMesContext _context;
     private readonly UserController _controller;
-    private List<UserDTO> _users;
+    private readonly Internacionalization _internacionalization;
+    private readonly UserValidations _userValidations;
+    private readonly List<UserDTO> _users;
+    private readonly InternacionalizationLanguage _language;
 
 
     public UserControllerTests()
@@ -18,12 +21,12 @@ public class UserControllerTests
         .UseInMemoryDatabase(Guid.NewGuid().ToString())
         .Options;
 
-        _context = new ComprasDoMesContext(options);
-        _controller = new UserController(_context);
-
-        _users = new();
-
-        _users.Add(
+        _context = new(options);
+        _internacionalization = new();
+        _userValidations = new();
+        _controller = new(_context, _internacionalization, _userValidations);
+        _users =
+        [
             new UserDTO
             {
                 Id = "1",
@@ -32,9 +35,7 @@ public class UserControllerTests
                 Password = "Str1ng@_s213123",
                 Birthdate = new DateOnly(1995, 9, 4)
             }
-        );
-
-        _users.Add(
+,
             new UserDTO
             {
                 Id = "2",
@@ -43,7 +44,8 @@ public class UserControllerTests
                 Password = "Str1ng@s213123",
                 Birthdate = new DateOnly(1995, 9, 4)
             }
-        );
+        ];
+        _language = InternacionalizationLanguage.Pt_BR;
     }
 
     
@@ -54,13 +56,13 @@ public class UserControllerTests
         UserDTO userDTO = _users[0];
 
         // Act
-        var result = await _controller.PostUser(userDTO);
+        var result = await _controller.PostUser(userDTO, InternacionalizationLanguage.Pt_BR);
 
         // Assert
         var actionResult = Assert.IsType<ActionResult<UserDTO>>(result);
         var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(actionResult.Result);
         var returnValue = Assert.IsType<UserDTO>(createdAtActionResult.Value);
-        Assert.Equal(userDTO.Id, returnValue.Id);
+        Assert.Equal(userDTO.Email, returnValue.Email);
 
     }
 
@@ -96,12 +98,9 @@ public class UserControllerTests
     public async Task GetUserById_ReturnsUser_WhenUserExists()
     {
         // Arrange
-        string id = "1";
-
         var userDTO = _users[0];
         var user = new User
         {
-            Id = userDTO.Id,
             Name = userDTO.Name,
             Email = userDTO.Email,
             Birthdate = userDTO.Birthdate,
@@ -111,15 +110,16 @@ public class UserControllerTests
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
 
+        string id = user.Id;
 
         // Act
-        var result = await _controller.GetUserById(id);
+        var result = await _controller.GetUserById(id, _language);
 
         // Assert
         var actionResult = Assert.IsType<ActionResult<UserDTO>>(result);
         var returnValue = Assert.IsType<OkObjectResult>(actionResult.Result);
         var _userDTO = Assert.IsType<UserDTO>(returnValue.Value);
-        Assert.Equal(userDTO.Id, _userDTO.Id);
+        Assert.Equal(id, _userDTO.Id);
     }
 
     [Fact]
@@ -129,10 +129,10 @@ public class UserControllerTests
         string id = "0";
 
         // Act
-        var result = await _controller.GetUserById(id);
+        var result = await _controller.GetUserById(id, _language);
 
         // Assert
-        Assert.IsType<NotFoundResult>(result.Result);
+        Assert.IsType<NotFoundObjectResult>(result.Result);
     }
 
     [Fact]
@@ -156,7 +156,7 @@ public class UserControllerTests
 
 
         // Act
-        var result = await _controller.GetUserByEmail(email);
+        var result = await _controller.GetUserByEmail(email, _language);
 
         // Assert
         var actionResult = Assert.IsType<ActionResult<UserDTO>>(result);
@@ -172,10 +172,10 @@ public class UserControllerTests
         string email = "qualqueremailquenaoexiste@issomesmo.enois";
 
         // Act
-        var result = await _controller.GetUserByEmail(email);
+        var result = await _controller.GetUserByEmail(email, _language);
 
         // Assert
-        Assert.IsType<NotFoundResult>(result.Result);
+        Assert.IsType<NotFoundObjectResult>(result.Result);
     }
 
     [Fact]
@@ -185,7 +185,6 @@ public class UserControllerTests
         UserDTO userDTO = _users[0];
         var user = new User
         {
-            Id = userDTO.Id,
             Name = userDTO.Name,
             Email = userDTO.Email,
             Birthdate = userDTO.Birthdate,
@@ -197,19 +196,20 @@ public class UserControllerTests
 
         string newEmail = "novoemail@email.com";
         
-        UserEmailDTO userEmailDTO = new UserEmailDTO{
+        UserEmailDTO userEmailDTO = new UserEmailDTO
+        {
             Email = newEmail
         };
 
         // When
-        var result = await _controller.UpdateUserEmail(user.Id, userEmailDTO);
+        var result = await _controller.UpdateUserEmail(user.Id, userEmailDTO, _language);
     
         // Then
         Assert.IsType<NoContentResult>(result.Result);
     }
 
     [Fact]
-    public async Task UpdateUserEmail_ReturnsNoContent_WhenUserDoesNotExists()
+    public async Task UpdateUserEmail_ReturnsNotFound_WhenUserDoesNotExists()
     {
         // Given
         UserDTO userDTO = _users[0];
@@ -221,10 +221,10 @@ public class UserControllerTests
         };
 
         // When
-        var result = await _controller.UpdateUserEmail(userDTO.Id, userEmailDTO);
+        var result = await _controller.UpdateUserEmail(userDTO.Id, userEmailDTO, _language);
 
         // Then
-        Assert.IsType<NotFoundResult>(result.Result);
+        Assert.IsType<NotFoundObjectResult>(result.Result);
     }
 
     [Fact]
@@ -251,7 +251,7 @@ public class UserControllerTests
         };
 
         // When
-        var result = await _controller.UpdateUserBirthDate(userDTO.Id, userBirthdateDTO);
+        var result = await _controller.UpdateUserBirthDate(user.Id, userBirthdateDTO, _language);
     
         // Then
         Assert.IsType<NoContentResult>(result.Result);
@@ -270,9 +270,9 @@ public class UserControllerTests
         };
 
         // When
-        var result = await _controller.UpdateUserBirthDate(userDTO.Id, userBirthdateDTO);
+        var result = await _controller.UpdateUserBirthDate(userDTO.Id, userBirthdateDTO, _language);
 
         // Then
-        Assert.IsType<NotFoundResult>(result.Result);
+        Assert.IsType<NotFoundObjectResult>(result.Result);
     }
 }
